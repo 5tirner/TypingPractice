d17efd37886da115934efe5afd03c3e5593a0f1d
5tirner <thatfor1337@gmail.com>
2024-09-26 18:04:18 +0100

    use angular.module

---
+++ front-end/code/structure.js
@@ -0,0 +1,10 @@
+
+const element = angular.module('elements', []);
+element.controller
+(   'controle',
+    function($scope)
+    {
+        $scope.intra_login = "zasabri";
+        $scope.data = "";
+    }
+);
